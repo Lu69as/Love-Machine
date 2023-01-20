943a3ff95375c3392826b00fe154091beaa4b064
Lu69as <Lukasokken@gmail.com>
2023-01-20 16:48:55 +0100

    lukas

--- js/main.js
+++ js/main.js
@@ -6,18 +6,25 @@ var love1 = prompt("Who are the first potential Lover?");
 var firstc1 = love1.slice(0,1)
 var love2 = prompt("And who are the second?")
 var firstc2 = love2.slice(0,1)
-var Anders = false;
+var Lukas = false;
+var Lukas = id = Lukas;
 var Theodor = false;
-if (love1 === "Theodor" || love2 === "Theodor" || love1 === "theodor" || love2 === "theodor" && Anders === false) {
+var Anders = false;
+
+if (love1 === "Lukas" || love2 === "Lukas" || love1 === "lukas" || love2 === "lukas" && Anders === false && Theodor === false) {
+    Lukas = true;
+    alert ("The Match Percentage Between " + firstc1.toUpperCase(0,1) + love1.slice(1,50) + " + " + firstc2.toUpperCase(0,1) + love2.slice(1,50) + " = " + -7432,638 + " % " + " ");
+} if (love1 === "Theodor" || love2 === "Theodor" || love1 === "theodor" || love2 === "theodor" && Lukas === false && Anders === false) {
     Theodor = true;
-    alert ("The Match Percentage Between " + firstc1.toUpperCase(0,1) + love1.slice(1,50) + " + " + firstc2.toUpperCase(0,1) + love2.slice(1,50) + " = " + 0.420 + " % " + " No bitches?");
-}
-if (love1 === "Anders" || love2 === "Anders" || love1 === "anders" || love2 === "anders" && Theodor === false) {
+    alert ("The Match Percentage Between " + firstc1.toUpperCase(0,1) + love1.slice(1,50) + " + " + firstc2.toUpperCase(0,1) + love2.slice(1,50) + " = " + 0.69 + " % " + " No bitches?");
+} if (love1 === "Anders" || love2 === "Anders" || love1 === "anders" || love2 === "anders" && Lukas === false && Theodor === false) {
     Anders = true;
     alert ("The Match Percentage Between " + firstc1.toUpperCase(0,1) + love1.slice(1,50) + " + " + firstc2.toUpperCase(0,1) + love2.slice(1,50) + " = " + 100 + " % " + " Literal Love Perfection");
 }
 
-if (Anders === false && Theodor === false) {
+
+
+if (Lukas === false && Anders === false && Theodor === false) {
     if (n > 70){
         alert ("The Match Percentage Between " + firstc1.toUpperCase(0,1) + love1.slice(1,50) + " + " + firstc2.toUpperCase(0,1) + love2.slice(1,50) + " = " + n + " % " + "ITS A MATCH MADE IN HEAVEN!");
     } if (n > 10 && n < 40) {
