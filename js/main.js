706ab3e26bee7fb714a0d0480f17a43480bbeb3d
Lu69as <Lukasokken@gmail.com>
2023-01-30 14:32:31 +0100

    neun und neunzig

--- js/main.js
+++ js/main.js
@@ -1,37 +1,72 @@
-function love_generation(){
-var n = Math.random();
-n = n * 100;
-n = Math.floor(n) + 1;
-var love1 = prompt("Who are the first potential Lover?");
-var firstc1 = love1.slice(0,1)
-var love2 = prompt("And who are the second?")
-var firstc2 = love2.slice(0,1)
-var Lukas = false;
-var Lukas = id = Lukas;
-var Theodor = false;
-var Anders = false;
+function love_generation() {
+    var n = Math.random();
+    n = n * 100;
+    n = Math.floor(n) + 1;
+    var name1 = prompt("Who is the first potential Lover?");
+    var name2 = prompt("And who is the second?");
 
-if (love1 === "Lukas" || love2 === "Lukas" || love1 === "lukas" || love2 === "lukas" && Anders === false && Theodor === false) {
-    Lukas = true;
-    alert ("Between Percentage The Match" + firstc1.toUpperCase(0,1) + love1.slice(1,50) + " + " + firstc2.toUpperCase(0,1) + love2.slice(1,50) + " = " + -7432,638 + " % " + " ahigyohobalishi");
-} if (love1 === "Theodor" || love2 === "Theodor" || love1 === "theodor" || love2 === "theodor" && Lukas === false && Anders === false) {
-    Theodor = true;
-    alert ("The Match Percentage Between " + firstc1.toUpperCase(0,1) + love1.slice(1,50) + " + " + firstc2.toUpperCase(0,1) + love2.slice(1,50) + " = " + 0.69 + " % " + " No bitches?");
-} if (love1 === "Anders" || love2 === "Anders" || love1 === "anders" || love2 === "anders" && Lukas === false && Theodor === false) {
-    Anders = true;
-    alert ("The Match Percentage Between " + firstc1.toUpperCase(0,1) + love1.slice(1,50) + " + " + firstc2.toUpperCase(0,1) + love2.slice(1,50) + " = " + 100 + " % " + " Literal Love Perfection");
-}
-if (Lukas === false && Anders === false && Theodor === false) {
-    if (n > 70){
-        alert ("The Match Percentage Between " + firstc1.toUpperCase(0,1) + love1.slice(1,50) + " + " + firstc2.toUpperCase(0,1) + love2.slice(1,50) + " = " + n + " % " + "ITS A MATCH MADE IN HEAVEN!");
-    } if (n > 10 && n < 40) {
-        alert ("The Match Percentage Between " + firstc1.toUpperCase(0,1) + love1.slice(1,50) + " + " + firstc2.toUpperCase(0,1) + love2.slice(1,50) + " = " + n + " % " + " Now this is awkward");
-    } if (n > 40 && n < 50) {
-        alert ("The Match Percentage Between " + firstc1.toUpperCase(0,1) + love1.slice(1,50) + " + " + firstc2.toUpperCase(0,1) + love2.slice(1,50) + " = " + n + " % " + " You should Stay friends");
-    } if (n < 70 && n > 50) {
-        alert ("The Match Percentage Between " + firstc1.toUpperCase(0,1) + love1.slice(1,50) + " + " + firstc2.toUpperCase(0,1) + love2.slice(1,50) + " = " + n + " % " + " Potential Lovebirds?");
-    } if (n < 10) {
-        alert ("The Match Percentage Between " + firstc1.toUpperCase(0,1) + love1.slice(1,50) + " + " + firstc2.toUpperCase(0,1) + love2.slice(1,50) + " = " + n + " % " + " Stay away from each other");
+    var love1 = name1.toLowerCase()
+    var love2 = name2.toLowerCase()
+    
+    var firstc1 = love1.slice(0, 1);
+    var firstc2 = love2.slice(0, 1);
+
+    if (love1 === "lukas" || love2 === "lukas") {
+        alert("Downsyndrome detected." + "Between Percentage Of The Match " + name1 + " + " + name2 + " = " + "-7432 638,00000000127" + " ¤ " + " Boobadibie Sjjoopphahaha");
+        alert("ALERT!! ERROR; EIIOU#");
+        prompt("WHAT IS NEM`'?£");
+        alert("Exiting Software");
+        window.close();
+    } 
+    else if (love1 === "theodor" || love2 === "theodor") {
+        alert("The Match Percentage Between " + name1 + " + " + name2 + " = " + 0.69 + " % " + " No bitches?");
+        alert("You may now leave the site");
+        alert("You didnt leave");
+        alert("You couldnt have seen this if you left!");
+        alert("You dont have a say in this!");
+        alert("You dont think I can close the tab?");
+        alert("I can actually!, \n But if you answer this question correct, you may stay.");
+        var nineplusten = prompt("What is 9 + 10?");
+        if (nineplusten === "21") {
+
+            alert("Fine! You can stay for this time...");
+        } else if (nineplusten === "19") {
+            alert("WRONG!!!");
+            alert("19 is completely wrong! Bad Math!");
+            window.close();
+        } else {
+            alert("Ha! Idiot");
+            alert("Didnt even get close lol!")
+            alert("Get the F*ck of my site");
+            window.close();
+        }
+    } 
+    else if (love1 === "anders" || love2 === "anders") {
+        alert("The Match Percentage Between " + name1 + " + " + name2 + " = " + 100 + " % " + " Literal Love Perfection");
+    } 
+    else if (love1 === "albert" || love2 === "albert") {
+        var firstc1 = love1.slice(0, 1);
+        var firstc2 = love2.slice(0, 1);
+        alert("The Match Percentage Between " + name1 + " + " + name2 + " = " + 0 + " % " + " Its allways gonna be zero, sorry Albert");
+        window.close();
+
+    } 
+    else {
+        var mp = "The Match Percentage Between " + name1 + " + " + name2 + " = " + n + " % "
+        if (n > 70) {
+            alert(mp + "ITS A MATCH MADE IN HEAVEN!");
+        }
+        else if (n > 50) {
+            alert(mp + " Potential Lovebirds?");
+        }
+        else if (n > 40) {
+            alert(mp + " You should Stay friends");
+        }
+        else if (n > 20) {
+            alert(mp + " Now this is awkward");
+        } 
+        else if (n < 10) {
+            alert(mp + " Stay away from each other");
+        }
     }
-}
 }
