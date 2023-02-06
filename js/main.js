53ea7a476ec786a013b3bc8bbd2b38c08471f657
Lu69as <lukasokken@gmail.com>
2023-02-06 09:54:08 +0100

    Le funny

--- js/main.js
+++ js/main.js
@@ -1,91 +1,105 @@
+var ShitAud = new Audio("../audio/Poop.mp3")
+var HackerE = new Audio("../audio/Hacking effect.mp3")
+var HackerV = new Audio("../audio/Hacker voice.mp3")
+var Rome = new Audio("../audio/Romantic.mp3")
+var Perfect = new Audio("../audio/Perfect.mp3")
+var Bass = new Audio("../audio/Bass.mp3")
+var Good = new Audio("../audio/Good.mp3")
 idx = 0;
-var L = 10;
-var T = 10
-function Popup(url, id) {
+function Popup(url, id, height, width, top, left) {
     popupWindow = window.open(
-        url, 'popUpWindow' + idx, 'height=400,width=500,left=L,top=T,resizable=no,scrollbars=no,toolbar=no,menubar=no,location=no,directories=no,status=yes');
+        url, 'popUpWindow' + idx, 'height=' + height + ',width=' + width + ',top=' + top + ',left=' + left + ',resizable=no,scrollbars=no,toolbar=no,menubar=no,location=no,directories=no,status=yes');
     idx += 1;
 }
-
-
-
 function love_generation() {
-    while (idx <= 5) {
-        Popup("https://www.tutorialspoint.com/basics_of_computer_science/images/computer_virus.jpg");
-        L + 100;
-        L + 100;
-        // Window.close.close();
+    var n = Math.random();
+    n = n * 100;
+    n = Math.floor(n) + 1;
+    var name1 = prompt("Who is the first potential Lover?");
+    var name2 = prompt("And who is the second?");
+
+    var love1 = name1.toLowerCase()
+    var love2 = name2.toLowerCase()
+
+    var firstc1 = love1.slice(0, 1);
+    var firstc2 = love2.slice(0, 1);
+
+    if (love1 === "lukas" || love2 === "lukas") {
+        alert("Downsyndrome detected." + "Between Percentage Of The Match " + name1 + " + " + name2 + " = " + "-7432 638,00000000127 ¤ Boobadibie Sjjoopphahaha");
+        alert("ALERT!! ERROR; EIIOU#");
+        prompt("WHAT IS NEM`'?£");
+        alert("Exiting Software");
+        HackerE.play();
+        alert("Virus Detected!! Brace for Impact!")
+        Popup("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.a_KNsxfVqL2a6t-Mo1hgBQHaD1%26pid%3DApi&f=1&ipt=d3d51ab3ad7b77122027f8279701f50172f886f21fe0cc6107454d105ca15889&ipo=images", 1, 400, 500, 200, 100);
+        Popup("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.NvlrZvK4-2_J2M_GtH7JNgHaEK%26pid%3DApi&f=1&ipt=9df74fc29b173da3a4e93d1e3d4e5c3c0259511cb780b8307da77989df2d2a3c&ipo=images", 2, 400, 500, 250, 150);
+        Popup("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.QNPgkYt2F_C9LRVHiT3nLQHaFj%26pid%3DApi&f=1&ipt=4daab259b6cd43122322d20e81a90b6c9cb1335d018b90e009ffd943676581b4&ipo=images", 3, 400, 500, 300, 200);
+        HackerV.play();
+        Popup("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.q0ieB9oPEZgC8GTVDHjISQAAAA%26pid%3DApi&f=1&ipt=75a1d506071bd63f930fb58914982cc2f75194ae756bbbaef55fc1528450db21&ipo=images", 4, 400, 500, 300, 300);
+        Popup("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.HX1X6OLpEktnaNalDjXfDgHaE8%26pid%3DApi&f=1&ipt=5fc9da2f1d30970034ff54eaf4a13feaae824077f2ffe78977eb0d8d1956750c&ipo=images", 5, 400, 500, 250, 350);
+        Popup("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.bj-3w8PAOZB7_rXMX1x8WwHaEK%26pid%3DApi&f=1&ipt=9c76eb4fdbe2fc0640972ad1b2c2c6854aff2676e6ff123f33087d98360a7e15&ipo=images", 6, 400, 500, 200, 420);
+        Popup("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.uauG30oY0GDLpWrPyB8WiQHaFj%26pid%3DApi&f=1&ipt=cb20840e3fc8cafbea9ba8b1a614e3df75b20e78ca374b65f064ccdcc553013a&ipo=images", 7, 400, 500, 150, 500);
+        HackerV.play();
+        Popup("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.l2_JREysQ4crUb_F7yg5LAHaFV%26pid%3DApi&f=1&ipt=bb4a5c0e40858c2c505bfa3054c31709354931fe3e28d77df8b7a228f09833dd&ipo=images", 8, 400, 500, 125, 550);
+        Popup("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.a3rLoZ91xUp5K7EmBLkRGwHaHa%26pid%3DApi&f=1&ipt=9053af0bc26d6cce4efd0d70b54187bb750fffc08ebe06176c28c6f80df36cc7&ipo=images", 9, 400, 500, 100, 600);
+        HackerV.play();
+        Popup("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Zw4y8CVMoMcH8GIFzqOthwHaHZ%26pid%3DApi&f=1&ipt=49e3969bfefde8c5285f62d4fc1d13426c5b4e9bbf758a1edb853d3d076d090b&ipo=images", 10, 400, 500, 150, 690);
+        Popup("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.dqdIY_6-RGGDlYway634SgHaD4%26pid%3DApi&f=1&ipt=10ad86493764752daaedb6d06fdb438ed7f9a852908d215fefa35b6d5f5eaeb4&ipo=images", 11, 400, 500, 250, 750);
+        window.close();
     }
-    
-    // var n = Math.random();
-    // n = n * 100;
-    // n = Math.floor(n) + 1;
-    // var name1 = prompt("Who is the first potential Lover?");
-    // var name2 = prompt("And who is the second?");
+    else if (love1 === "theodor" || love2 === "theodor") {
+        ShitAud.play();
+        alert("The Match Percentage Between " + name1 + " + " + name2 + " = " + 0.69 + " % " + " No bitches?");
+        alert("You may now leave the site");
+        alert("You didnt leave");
+        alert("You couldnt have seen this if you left!");
+        alert("You dont have a say in this!");
+        alert("You dont think I can close the tab?");
+        alert("I can actually!, \n But if you answer this question correct, you may stay.");
+        var nineplusten = prompt("What is 9 + 10?");
+        if (nineplusten === "21") {
 
-    // var love1 = name1.toLowerCase()
-    // var love2 = name2.toLowerCase()
+            alert("Fine! You can stay for this time...");
+        } else if (nineplusten === "19") {
+            alert("WRONG!!!");
+            alert("19 is completely wrong! Bad Math!");
+            window.close();
+        } else {
+            alert("Ha! Idiot");
+            alert("Didnt even get close lol!")
+            alert("Get the F*ck of my site");
+            window.close();
+        }
+    }
+    else if (love1 === "anders" || love2 === "anders") {
+        Perfect.play();
+        alert("The Match Percentage Between " + name1 + " + " + name2 + " = " + 100 + " % " + " Literal Love Perfection");
+    }
+    else if (love1 === "albert" || love2 === "albert") {
+        Bass.play();
+        alert("The Match Percentage Between " + name1 + " + " + name2 + " = " + 0 + " % " + " Its allways gonna be zero, sorry Albert");
+        window.close();
 
-    // var firstc1 = love1.slice(0, 1);
-    // var firstc2 = love2.slice(0, 1);
-
-    // if (love1 === "lukas" || love2 === "lukas") {
-    //     alert("Downsyndrome detected." + "Between Percentage Of The Match " + name1 + " + " + name2 + " = " + "-7432 638,00000000127" + " ¤ " + " Boobadibie Sjjoopphahaha");
-    //     alert("ALERT!! ERROR; EIIOU#");
-    //     prompt("WHAT IS NEM`'?£");
-    //     alert("Exiting Software");
-    //     window.close();
-
-    // }
-    // else if (love1 === "theodor" || love2 === "theodor") {
-    //     alert("The Match Percentage Between " + name1 + " + " + name2 + " = " + 0.69 + " % " + " No bitches?");
-    //     alert("You may now leave the site");
-    //     alert("You didnt leave");
-    //     alert("You couldnt have seen this if you left!");
-    //     alert("You dont have a say in this!");
-    //     alert("You dont think I can close the tab?");
-    //     alert("I can actually!, \n But if you answer this question correct, you may stay.");
-    //     var nineplusten = prompt("What is 9 + 10?");
-    //     if (nineplusten === "21") {
-
-    //         alert("Fine! You can stay for this time...");
-    //     } else if (nineplusten === "19") {
-    //         alert("WRONG!!!");
-    //         alert("19 is completely wrong! Bad Math!");
-    //         window.close();
-    //     } else {
-    //         alert("Ha! Idiot");
-    //         alert("Didnt even get close lol!")
-    //         alert("Get the F*ck of my site");
-    //         window.close();
-    //     }
-    // }
-    // else if (love1 === "anders" || love2 === "anders") {
-    //     alert("The Match Percentage Between " + name1 + " + " + name2 + " = " + 100 + " % " + " Literal Love Perfection");
-    // }
-    // else if (love1 === "albert" || love2 === "albert") {
-    //     var firstc1 = love1.slice(0, 1);
-    //     var firstc2 = love2.slice(0, 1);
-    //     alert("The Match Percentage Between " + name1 + " + " + name2 + " = " + 0 + " % " + " Its allways gonna be zero, sorry Albert");
-    //     window.close();
-
-    // }
-    // else {
-    //     var mp = "The Match Percentage Between " + name1 + " + " + name2 + " = " + n + " % "
-    //     if (n > 70) {
-    //         alert(mp + "ITS A MATCH MADE IN HEAVEN!");
-    //     }
-    //     else if (n > 50) {
-    //         alert(mp + " Potential Lovebirds?");
-    //     }
-    //     else if (n > 40) {
-    //         alert(mp + " You should Stay friends");
-    //     }
-    //     else if (n > 20) {
-    //         alert(mp + " Now this is awkward");
-    //     }
-    //     else if (n < 10) {
-    //         alert(mp + " Stay away from each other");
-    //     }
-    // }
+    }
+    else {
+        var mp = "The Match Percentage Between " + name1 + " + " + name2 + " = " + n + " % "
+        if (n > 70) {
+            Rome.play();
+            alert(mp + "ITS A MATCH MADE IN HEAVEN!");
+        }
+        else if (n > 50) {
+            Good.play();
+            alert(mp + " Potential Lovebirds?");
+        }
+        else if (n > 40) {
+            alert(mp + " You should Stay friends");
+        }
+        else if (n > 20) {
+            alert(mp + " Now this is awkward");
+        }
+        else if (n < 10) {
+            ShitAud.play();
+            alert(mp + " Stay away from each other");
+        }
+    }
 }
